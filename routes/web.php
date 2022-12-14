d831613d4180c368a21b9bd9763bb50d463ec5e2
I made kamajaya <imadekamajaya@Is-MacBook-Pro.local>
2022-12-14 07:55:26 +0700

    pra login

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\MovieController;
 use App\Http\Controllers\Admin\TransactionController;
+use App\Http\Controllers\Admin\LoginController;
 
 
 /*
@@ -15,6 +16,8 @@ use App\Http\Controllers\Admin\TransactionController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('admin/login', [LoginController::class, 'index'])->name('admin.login');
 
 Route::group(['prefix' => 'admin'], function(){
     Route::view('/', 'admin.dashboard')->name('admin.dashboard');
